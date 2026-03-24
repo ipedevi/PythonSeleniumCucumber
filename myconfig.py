fa0d96465efb839404cb1bce74503a89914afb0e
Ivan del Viso <ivan.viso@zitrogames.com>
2026-03-24 19:29:09 +0100

    action v3

--- myconfig.py
+++ myconfig.py
@@ -2,7 +2,7 @@
 
 # Selenium Configuration
 SELENIUM_CONFIG = {
-    "browser": "chrome_remote",  # "chrome_local", "firefox_local", "chrome_remote", "firefox_remote"
+    "browser": "chrome_local",  # "chrome_local", "firefox_local", "chrome_remote", "firefox_remote"
     "headless": False,          # True for headless mode (local only)
     "initial_page": "https://demo.testim.io/",
     "remote_url": "http://selenium.qaserver01.com:4444/wd/hub"  # Selenium Grid URL (used when browser is *_remote)
